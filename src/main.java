1a1f15f9e8cf64ebe08d5fcc32d1b626f4746f92
lseguin <leoseg8888gmail.com>
2022-10-13 18:36:51 +0200

    Add main Film

--- src/main.java
+++ src/main.java
@@ -2,4 +2,11 @@ public class main {
     Client Ewan = new Client("Ewan",20);
     //hey
 
+
+
+    // Création des films
+    Film HarryPotter = new Film(25, "Harry Potter", "Bon état");
+    Film LOTR = new Film(45, "The Lord of the ring", "Très bon état");
+    Film Interstellar = new Film(28, "Interstellar", "Bon état");
+    Film Alad2 = new Film(5, "Alad'2", "Mauvais état");
 }
