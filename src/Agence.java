78e7feae45dec7291453cee3659133697bbaa67a
Ewan Gullaud <ewan.gullaud@gmail.com>
2022-10-13 18:32:31 +0200

    Coucou

--- src/Agence.java
+++ src/Agence.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Agence {
+
+
+
+        public static void main(String[] args) {
+            ArrayList<String> cars = new ArrayList<String>();
+            cars.add("Volvo");
+            cars.add("BMW");
+            cars.add("Ford");
+            cars.add("Mazda");
+            System.out.println(cars);
+        }
+
 }
