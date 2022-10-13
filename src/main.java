8ad9172b3e909f7f1378847de8f5959ef50d44fb
lseguin <leoseg8888gmail.com>
2022-10-13 18:37:29 +0200

    Add main Film

--- src/main.java
+++ src/main.java
@@ -6,7 +6,7 @@ public class main {
 
     // Création des films
     Film HarryPotter = new Film(25, "Harry Potter", "Bon état");
-    Film LOTR = new Film(45, "The Lord of the ring", "Très bon état");
+    Film LOTR = new Film(45, "The Lord of the Rings", "Très bon état");
     Film Interstellar = new Film(28, "Interstellar", "Bon état");
     Film Alad2 = new Film(5, "Alad'2", "Mauvais état");
 }
