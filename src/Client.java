ef680491998432f2ee428041cbdb18f3881e2046
Heiwa70 <clement70200@gmail.com>
2022-10-14 18:02:03 +0200

    Coucou

--- src/Client.java
+++ src/Client.java
@@ -8,10 +8,7 @@ public class Client {
     private String nom;
     private int age;
 
-    private Agence agence;
-    private List<String> Films = new ArrayList<>();
-    private List<String> Support = new ArrayList<>();
-    private List<Integer> DuréeLocation = new ArrayList<>();
+
 
 
 
@@ -20,9 +17,5 @@ public class Client {
         this.age = age;
     }
 
-    public void loue(String filmDemandé, String supportDemandé, int duréeDemandé){
-        this.Films.add(filmDemandé);
-        this.Support.add(supportDemandé);
-        this.DuréeLocation.add(duréeDemandé);
-    }
+
 }
