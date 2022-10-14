766a333b946f491d10f873b41bb48e3d3f95ed56
Heiwa70 <clement70200@gmail.com>
2022-10-14 18:10:16 +0200

    Coucou

--- src/Client.java
+++ src/Client.java
@@ -3,19 +3,13 @@ import java.util.List;
 
 public class Client {
 
-
-
     private String nom;
     private int age;
-
-
-
-
 
     public Client(String nom,int age){
         this.nom = nom;
         this.age = age;
     }
 
-
+// connard d'ewan
 }
--- src/main.java
+++ src/main.java
@@ -1,17 +1,15 @@
 public class main {
-    Client Ewan = new Client("Ewan",20);
-
-
+    Client Ewan = new Client("Ewan", 20);
 
 
     // Création des films
-    Film HarryPotter    = new Film(25, "Harry Potter", "Ancien", "Fantaisie");
-    Film LOTR           = new Film(45, "The Lord of the Rings", "Ancien", "Fantaisie");
-    Film Interstellar   = new Film(28, "Interstellar", "Récent", "ScienceFiction");
-    Film Alad2          = new Film(5, "Alad'2", "Récent", "Comédie");
-    Film Avatar         = new Film(32, "Avatar", "Ancien", "ScienceFiction");
-    Film Titanic        = new Film(38, "Titanic", "Ancien", "DrameRomantique");
-    Film StarWars       = new Film(38, "Start Wars", "Ancien", "ScienceFiction");
+    Film HarryPotter = new Film(25, "Harry Potter", "Ancien", "Fantaisie");
+    Film LOTR = new Film(45, "The Lord of the Rings", "Ancien", "Fantaisie");
+    Film Interstellar = new Film(28, "Interstellar", "Récent", "ScienceFiction");
+    Film Alad2 = new Film(5, "Alad'2", "Récent", "Comédie");
+    Film Avatar = new Film(32, "Avatar", "Ancien", "ScienceFiction");
+    Film Titanic = new Film(38, "Titanic", "Ancien", "DrameRomantique");
+    Film StarWars = new Film(38, "Start Wars", "Ancien", "ScienceFiction");
 
     Support HarryPoterDVD = new Support(true, HarryPotter, Support.typeSupport.DVD);
     Support TitanicK7 = new Support(true, Titanic, Support.typeSupport.K7Video);
