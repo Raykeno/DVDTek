c4924756640dd2926f39ca643bbe98b908c14c3e
Ewan Gullaud <ewan.gullaud@gmail.com>
2022-10-14 17:09:39 +0200

    Définition de DVD + getter et setters

--- src/Agence.java
+++ src/Agence.java
@@ -1,16 +1,13 @@
 import java.util.ArrayList;
 
 public class Agence {
-
+//Seiler
 
 
         public static void main(String[] args) {
             ArrayList<String> cars = new ArrayList<String>();
-            cars.add("Volvo");
-            cars.add("BMW");
-            cars.add("Ford");
-            cars.add("Mazda");
-            System.out.println(cars);
+
         }
+
 
 }
