def603730d94bc041ee5e51877c2dd80c2fbf350
Otto Hajdu <raythekeno@gmail.com>
2022-10-14 17:32:51 +0200

    Rajout d'exemples de support dans main

--- src/main.java
+++ src/main.java
@@ -12,4 +12,8 @@ public class main {
     Film Avatar         = new Film(32, "Avatar", "Ancien", "ScienceFiction");
     Film Titanic        = new Film(38, "Titanic", "Ancien", "DrameRomantique");
     Film StarWars       = new Film(38, "Start Wars", "Ancien", "ScienceFiction");
+
+    Support HarryPoterDVD = new Support(true, HarryPotter, Support.typeSupport.DVD);
+    Support TitanicK7 = new Support(true, Titanic, Support.typeSupport.K7Video);
+    Support Alad2BluRay = new Support(false, Alad2, Support.typeSupport.BluRay);
 }
