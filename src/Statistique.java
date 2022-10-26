2881851b47124f31820ae54aeb92b55de7992082
Ewan Gullaud <ewan.gullaud@gmail.com>
2022-10-26 17:40:24 +0200

    Coucou les Loulous

--- src/Statistique.java
+++ src/Statistique.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Statistique {
+
+    private String[] Support_Pref = {
+            "DVD",
+            "Blue-Ray",
+            "K7"
+    };
+
+    private String[] Genres ={
+            "Action",
+            "Comédie",
+            "Horreur",
+            "Romance",
+    };
+
+    private String[] Categories ={
+            "Récent",
+            "Ancien",
+
+    };
+
+    private List<String> Support = new ArrayList<>(Arrays.asList(Support_Pref));
+    private List<String> Genre = new ArrayList<>(Arrays.asList(Genres));
+    private List<String> Categorie = new ArrayList<>(Arrays.asList(Categories));
+
+
+
 }
