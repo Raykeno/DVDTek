85838aa283c385c2895b125a677c043b92771dc7
Heiwa70 <clement70200@gmail.com>
2022-10-26 17:44:23 +0200

    Coucou

--- src/Agence.java
+++ src/Agence.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Agence {
+
     private Film[] Film_connu = {
             new Film(15,"Harry Potter 7", "Ancien","SF"),
             new Film(20,"Plancha", "Récent","Comedie"),
@@ -31,7 +32,7 @@ public class Agence {
 
     private List<Film> Film = new ArrayList<>(Arrays.asList(Film_connu));
     private List<Client> ListClient = new ArrayList<>(Arrays.asList(Client_connu));
-
+//test
 
     private boolean isknow(Client client) {
         for (int i = 0; i < this.ListClient.size(); i++) {
