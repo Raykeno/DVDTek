970f311155e66688382c10b8832ad7fe3c742e9d
Heiwa70 <clement70200@gmail.com>
2022-10-26 17:32:20 +0200

    update class Agence.java

--- src/Agence.java
+++ src/Agence.java
@@ -3,33 +3,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Agence {
-    private String[] Film_existant = {
-            "Star wars",
-            "Retour vers le futur",
-            "Retour vers le futur 2",
-            "Retour vers le futur",
-            "Harry Potter"
-    };
-    private String[] Support_Dispo = {
-            "DVD",
-            "Blue-Ray",
-            "K7"
+    private Film[] Film_connu = {
+            new Film(15,"Harry Potter 7", "Ancien","SF"),
+            new Film(20,"Plancha", "Récent","Comedie"),
+            new Film(2,"Transformers 1", "Ancien","SF"),
+            new Film(5,"Transporteur 3", "Ancien","Action")
     };
 
 
-    private Client[] Client_existant = {
-            new Client("Clement",20),
-            new Client("Keke",20),
-            new Client("Paul",14),
-            new Client("Arthur",12)
+    private Client[] Client_connu = {
+            new Client("Clement", 20),
+            new Client("Keke", 20),
+            new Client("Paul", 14),
+            new Client("Arthur", 12)
     };
 
-    private List<String> Support = new ArrayList<>(Arrays.asList(Support_Dispo));
-    private List<String> Film = new ArrayList<>(Arrays.asList(Film_existant));
-    private List<Client> ListClient = new ArrayList<>(Arrays.asList(Client_existant));
+
+    public List<Film> getFilm() {
+        return Film;
+    }
+
+    public List<Client> getListClient() {
+        return ListClient;
+    }
 
 
-    private boolean isknow(Client client){
+
+    private List<Film> Film = new ArrayList<>(Arrays.asList(Film_connu));
+    private List<Client> ListClient = new ArrayList<>(Arrays.asList(Client_connu));
+
+
+    private boolean isknow(Client client) {
         for (int i = 0; i < this.ListClient.size(); i++) {
             if (this.ListClient.get(i).getNom() == client.getNom()) {
                 return true;
@@ -37,18 +41,6 @@ public class Agence {
         }
         return false;
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
