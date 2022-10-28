2446b0c8e28c23951bfb274822c1b7b58bdb9e70
lseguin <leoseg8888@gmail.com>
2022-10-28 17:53:21 +0200

    add Reglement

---
+++ src/Reglement.java
@@ -0,0 +1,27 @@
+//Léo
+public class Reglement {
+
+    private Client client;
+    private Facturation facturation;
+
+    public Reglement( Client client, Facturation facturation) {
+        this.client = client;
+        this.facturation = facturation;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Facturation getFacturation() {
+        return facturation;
+    }
+
+    public void setFacturation(Facturation facturation) {
+        this.facturation = facturation;
+    }
+}
